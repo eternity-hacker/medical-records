f40b72db11f1513369a8d5287a76587a8f3bae65
Olivia <eternity@hotlookbook.com>
2022-10-21 17:49:14 -0700

    modified icon from exclamation to checkmark

--- script.js
+++ script.js
@@ -3,10 +3,6 @@ const locations = document.querySelector(".locations");
 
 // these variables target my records-box
 const recordsBox = document.querySelector("#records-box");
-const personalIcon = document.querySelector("#personal-icon");
-const allergiesIcon = document.querySelector("#allergies-icon");
-const shotsIcon = document.querySelector("#shots-icon");
-const medicationsIcon = document.querySelector("#medications-icon");
 
 offices.addEventListener("click", function (event) {
   if (event.target.value === "office-1") {
@@ -16,10 +12,7 @@ offices.addEventListener("click", function (event) {
 
 // event listener for records-box
 recordsBox.addEventListener("click", function (event) {
-  if (event.target.tagName != "FIGURE") {
-    const figureElement = event.target.parentElement;
-    const iconElement = figureElement.children[0];
-    iconElement.className = "checkmark";
-  } else if (event.target.tagName === "FIGURE") {
+  if (event.target.tagName === "FIGURE") {
+    event.target.children[0].className = "checkmark";
   }
 });
