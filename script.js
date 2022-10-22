8e1a7976f0c91f7a5df23382939eb11b434f0510
Olivia <eternity@hotlookbook.com>
2022-10-21 20:02:41 -0700

    added event listener to my select button that displays office locations

--- script.js
+++ script.js
@@ -1,12 +1,19 @@
 const offices = document.querySelector("#offices");
-const locations = document.querySelector(".locations");
+const locations = document.querySelectorAll(".locations");
 
 // these variables target my records-box
 const recordsBox = document.querySelector("#records-box");
 
 offices.addEventListener("click", function (event) {
   if (event.target.value === "office-1") {
+    locations[1].style.display = "none"; //hides office 2
+    locations[0].style.display = "block";
   } else if (event.target.value === "office-2") {
+    locations[0].style.display = "none"; //hides office 1
+    locations[1].style.display = "block";
+  } else {
+    locations[0].style.display = "none";
+    locations[1].style.display = "none";
   }
 });
 
